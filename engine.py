075939843958a7c0d8a629bc4be0c9ba8ddb9c11
Lennart Mol <lennartmol@live.nl>
2025-10-12 16:18:23 +0200

    Game of life rules

--- engine.py
+++ engine.py
@@ -30,11 +30,30 @@ arr_test_count_2 = [
     [0, 0, 0, 0, 0]
 ]
 
+# array for testing count_neighbours
+arr_test_count_3 = [
+    [0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0]
+]
 
-for row in arr_test_count_2:
-    print(row)
+# array for testing count_neighbours
+arr_test_count_4 = [
+    [0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
+]
 
 selected_cell = [2,2]
+selected_array = arr_test_count_4
+
+for row in selected_array:
+    print(row)
+
 
 # rules
 # Any live cell with fewer than two live neighbours dies, as if by underpopulation.
@@ -56,4 +75,37 @@ def count_neighbours(arr, target_cell):
     return total_neighbours
 
 
-print(count_neighbours(arr_test_count_2, selected_cell))
+def decide_fate_cell(arr, target_cell, number_of_neighbours):
+    targetRow = target_cell[0]
+    targetCol = target_cell[1]
+
+
+    # question? do you exist
+    if (arr[targetRow][targetCol]):
+        # yes
+
+        # rule 1: thou shall cease if they neighbours are fewer than 2
+        if (number_of_neighbours < 2):
+            arr[targetRow][targetCol] = 0
+        # rule 2: thou shall live this generation if they neighbours are 2 or 3
+        if (number_of_neighbours >= 2 and number_of_neighbours <= 3 ):
+            return
+        # rule 3: thou shall case if they neighbours are greater than 3
+        if (number_of_neighbours > 3): 
+            arr[targetRow][targetCol] = 0
+    
+    else:
+        # no
+
+        # do you want to live?
+        # rule 4: thou shall be give the essence of the almighty '1' if they neighbours are EXACTLY 3
+        if (number_of_neighbours == 3):
+            arr[targetRow][targetCol] = 1
+
+number_of_neighbours = count_neighbours(selected_array, selected_cell)
+print(number_of_neighbours)
+
+decide_fate_cell(selected_array, selected_cell, number_of_neighbours)
+
+for row in selected_array:
+    print(row)
