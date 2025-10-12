27fdaba0125188135b49bc451f34433b47eda05f
Lennart Mol <lennartmol@live.nl>
2025-10-12 17:12:14 +0200

    Fixed bug decide fate cell with new array implementation

--- engine.py
+++ engine.py
@@ -30,24 +30,24 @@ def count_neighbours(arr, target_cell):
     return total_neighbours
 
 
-def decide_fate_cell(arr, target_cell, number_of_neighbours):
+def decide_fate_cell(newArr, oldArr, target_cell, number_of_neighbours):
     targetRow = target_cell[0]
     targetCol = target_cell[1]
 
 
     # question? do you exist
-    if (arr[targetRow][targetCol]):
+    if (oldArr[targetRow][targetCol]):
         # yes
 
         # rule 1: thou shall cease if they neighbours are fewer than 2
         if (number_of_neighbours < 2):
-            arr[targetRow][targetCol] = 0
+            newArr[targetRow][targetCol] = 0
         # rule 2: thou shall live this generation if they neighbours are 2 or 3
         if (number_of_neighbours >= 2 and number_of_neighbours <= 3 ):
-            return
+            newArr[targetRow][targetCol] = 1
         # rule 3: thou shall case if they neighbours are greater than 3
         if (number_of_neighbours > 3): 
-            arr[targetRow][targetCol] = 0
+            newArr[targetRow][targetCol] = 0
     
     else:
         # no
@@ -55,7 +55,7 @@ def decide_fate_cell(arr, target_cell, number_of_neighbours):
         # do you want to live?
         # rule 4: thou shall be give the essence of the almighty '1' if they neighbours are EXACTLY 3
         if (number_of_neighbours == 3):
-            arr[targetRow][targetCol] = 1
+            newArr[targetRow][targetCol] = 1
 
 def loop_through_array(old_array):
     rows = len(old_array)
@@ -67,7 +67,7 @@ def loop_through_array(old_array):
             number_of_neighbours = count_neighbours(old_array, selected_cell)
             print(number_of_neighbours)
 
-            decide_fate_cell(new_array, selected_cell, number_of_neighbours)
+            decide_fate_cell(new_array, old_array, selected_cell, number_of_neighbours)
             for row in new_array:
                 print(row)
 
