32f053a5c2bd18e17dfd7080232c51e048510990
Lennart Mol <lennartmol@live.nl>
2025-10-17 10:24:17 +0200

    Implemented batch rendering (360ms -> 15ms)

--- window.py
+++ window.py
@@ -21,36 +21,38 @@ class Window():
     
         self.debug_state = debug
 
-
-        self.rectangles = []
-        self.create_rectangles()
+        self.batch = pyglet.graphics.Batch()
+        self.cells = self.create_batch_cells()
 
         self.window.push_handlers(on_draw=self.on_draw)
 
     def on_draw(self):
         if(self.debug_state):
             start = time.time()
-        self.draw_window()
+
+        self.window.clear()
+
+        self.update_cell_positions()
+        self.batch.draw()
         
         if(self.debug_state):
             print(f"Drawing took {(time.time() - start)*1000:.2f} ms")
         
     
-    def draw_window(self):
-        self.window.clear()
-        for i, rect in enumerate(self.rectangles):
+    def update_cell_positions(self):
+        for i, cell in enumerate(self.cells):
             row = i // self.number_of_columns
             col = i % self.number_of_columns
-            rect.color = (255, 255, 255) if self.game_engine.old_generation_array[row][col] else (0, 0, 0)
-            rect.draw()
-        self.window.invalid = True
+            cell.color = (255, 255, 255) if self.game_engine.old_generation_array[row][col] else (0, 0, 0)
 
-    def create_rectangles(self):
-        self.rectangles = []
+    def create_batch_cells(self):
+        cells = []
         for row in range(self.number_of_rows):
             for col in range(self.number_of_columns):
                 x = col * self.cell_size
                 y = row * self.cell_size
                 color = (255, 255, 255) if self.game_engine.old_generation_array[row][col] else (0, 0, 0)
-                rect = pyglet.shapes.Rectangle(x, y, self.cell_size, self.cell_size, color=color)
-                self.rectangles.append(rect)
+                cell = pyglet.shapes.Rectangle(x, y, self.cell_size, self.cell_size, color=color, batch=self.batch)
+                cells.append(cell)
+        
+        return cells
